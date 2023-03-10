affa8fdbfd34a508431518f97084ef014c462ebf
jinhyunni <pjhhhk@gmail.com>
2023-03-10 12:42:36 +0900

    R_pAu updated in progress

--- Git_Bias_correction_RxA/R_pAu.cpp
+++ Git_Bias_correction_RxA/R_pAu.cpp
@@ -26,20 +26,16 @@ void R_pAu()
 {
 	//read in files
 	TFile* avgNcoll = new TFile("pAu200GeV_option3_INEL_avgNcoll.root", "read");
-	TFile* pAuYield = new TFile("pAu200GeV_option3_pion0Analysis_etacut_INEL_1e5.root", "read");
+	TFile* pAuYield = new TFile("pAu200GeV_option3_pion0Analysis_invarY1e5.root", "read");
+
 	TFile* refYield = new TFile("pAu200GeV_option3_pion0Analysis_Ncoll1_etacut_1e5.root", "read");
+	TH1D* reference = (TH1D*)refYield -> Get("pTpion0")
 
-	//Invariant yield for class 1
-	TH2D* h2pTcent = (TH2D*)pAuYield -> Get("pTpion0_cent");
-	TH2D* h2ncollcent = (TH2D*)pAuYield -> Get("ncoll_cent");
-
-	TH1D* pAuClass1 = (TH1D*)h2pTcent -> ProjectionY("pAuClass1", 1, 1);
-	TH1D* h1cent = (TH1D*)h2ncollcent -> ProjectionX("h1cent");
-
-	double scalar1 = 1./(pAuClass1 -> GetBinWidth(1)*h1cent -> GetBinContent(1));
-	pAuClass1 -> Scale(scalar1);
-
-	//
+	
+	//R_pAu of centrality class 1(0~10%)
+	////Get p+Au Invariant yield
+	TH1D* pAuClass1 = (TH1D*)pAuYield -> Get("pTclass1");
+	
 
 
 }
