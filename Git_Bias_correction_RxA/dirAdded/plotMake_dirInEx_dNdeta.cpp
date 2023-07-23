1a5077ef9c062f6019b81ae25ca55d8880b276c2
jinhyunni <pjhhhk@gmail.com>
2023-07-24 08:22:20 +0900

    init

--- Git_Bias_correction_RxA/dirAdded/plotMake_dirInEx_dNdeta.cpp
+++ Git_Bias_correction_RxA/dirAdded/plotMake_dirInEx_dNdeta.cpp
@@ -1,13 +1,13 @@
 void plotMake_dirInEx_dNdeta()
 {
-	TFile *input1 = new TFile("pAu200GeV_option3_dirAdded_dNdeta_1e5events.root", "read");
+	TFile *input1 = new TFile("pAu200GeV_option3_dirAdded_decayOff_dNdeta_1e5Events.root", "read");
 	TFile *input2 = new TFile("pAu200GeV_option3_dirExcluded_dNdeta_1e5events.root", "read");
 
 	TH1D *new_pionEta = (TH1D*)input1 -> Get("dirAdded_pionEta");
 	TH1D *new_kaonEta = (TH1D*)input1 -> Get("dirAdded_kaonEta");
 	TH1D *new_protonEta = (TH1D*)input1 -> Get("dirAdded_protonEta");
 
-	TH1D*new_charged = new TH1D();
+	TH1D *new_charged = new TH1D();
 	new_charged -> Sumw2();
 	(*new_charged) = (*new_pionEta) + (*new_kaonEta) + (*new_protonEta);
 
@@ -15,7 +15,7 @@ void plotMake_dirInEx_dNdeta()
 	TH1D *old_kaonEta = (TH1D*)input2 -> Get("dirEx_kaonEta");
 	TH1D *old_protonEta = (TH1D*)input2 -> Get("dirEx_protonEta");
 
-	TH1D*old_charged = new TH1D();
+	TH1D *old_charged = new TH1D();
 	old_charged -> Sumw2();
 	(*old_charged) = (*old_pionEta) + (*old_kaonEta) + (*old_protonEta);
 
@@ -43,7 +43,7 @@ void plotMake_dirInEx_dNdeta()
 		htmp -> GetXaxis() -> SetTitle("#eta");
 		htmp -> GetYaxis() -> SetTitle("invariant yield");
 
-		new_charged -> SetMarkerStyle(28);
+		new_charged -> SetMarkerStyle(20);
 		new_charged -> SetMarkerColor(2);
 		new_charged -> SetLineColor(2);
 		new_charged -> Draw("p same");
@@ -60,7 +60,7 @@ void plotMake_dirInEx_dNdeta()
 		leg1 -> AddEntry("","PYTHIA8, option3", "h");
 		leg1 -> AddEntry("","#pi^{#pm}/K^{#pm}/p^{#pm}", "h");
 		leg1 -> AddEntry("","100,000 events", "h");
-		leg1 -> AddEntry(new_charged, "#gamma included file", "p");
+		leg1 -> AddEntry(new_charged, "#gamma included file, decayOff", "p");
 		leg1 -> AddEntry(old_charged, "#gamma excluded file", "p");
 
 		leg1 -> Draw();
