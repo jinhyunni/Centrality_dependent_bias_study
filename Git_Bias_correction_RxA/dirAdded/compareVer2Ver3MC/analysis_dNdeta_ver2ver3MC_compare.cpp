6b9afe0e82bba9f4674d820413a85cda1d78a823
jinhyunni <pjhhhk@gmail.com>
2023-09-18 19:27:09 +0900

    dndeta files modified

--- Git_Bias_correction_RxA/dirAdded/compareVer2Ver3MC/analysis_dNdeta_ver2ver3MC_compare.cpp
+++ Git_Bias_correction_RxA/dirAdded/compareVer2Ver3MC/analysis_dNdeta_ver2ver3MC_compare.cpp
@@ -17,6 +17,9 @@ void analysis_dNdeta_ver2ver3MC_compare()
     TFile *ver3_3e7events_yield = new TFile("pAu200GeV_kiaf_option3_TH2DpTeta_3e7MBevents.root", "read");
     TFile *ver3_3e7events_nEvent = new TFile("pAu200GeV_kiaf_option3_Ncoll_3e7MBevents.root", "read");
 
+    TFile *ver3_tmp_yield = new TFile("pAu200GeV_kiaf_ver3tmp_option3_TH2DpTeta_MBevents.root", "read");
+    TFile *ver3_tmp_nEvent = new TFile("pAu200GeV_kiaf_ver3tmp_option3_Ncoll_MBevents.root", "read");
+
     //TH2D pTeta of pion0/dirphoton
     TH2D *h2pTeta_pion0_ver2 = (TH2D*)ver2_yield -> Get("pTetaPion0_mb");
     TH2D *h2pTeta_dir_ver2 = (TH2D*)ver2_yield -> Get("pTetaDir_mb");
@@ -32,6 +35,10 @@ void analysis_dNdeta_ver2ver3MC_compare()
 
     TH2D *h2pTeta_pion0_ver3_3e7events = (TH2D*)ver3_3e7events_yield -> Get("pTetaPion0_mb");
     TH2D *h2pTeta_dir_ver3_3e7events = (TH2D*)ver3_3e7events_yield -> Get("pTetaDir_mb");
+
+    TH2D *h2pTeta_pion0_ver3_tmp = (TH2D*)ver3_tmp_yield -> Get("pTetaPion0_mb");
+    TH2D *h2pTeta_dir_ver3_tmp = (TH2D*)ver3_tmp_yield -> Get("pTetaDir_mb");
+
     
     //TH1D ncoll
     TH1D *ncoll_ver2 = (TH1D*)ver2_nEvent -> Get("ncoll_mb");
@@ -39,6 +46,7 @@ void analysis_dNdeta_ver2ver3MC_compare()
     TH1D *ncoll_ver3_test = (TH1D*)ver3_test_nEvent -> Get("ncoll_mb");
     TH1D *ncoll_ver3_test_2e7events = (TH1D*)ver3_test_2e7events_nEvent -> Get("ncoll_mb");
     TH1D *ncoll_ver3_3e7events = (TH1D*)ver3_3e7events_nEvent -> Get("ncoll_mb");
+    TH1D *ncoll_ver3_tmp = (TH1D*)ver3_tmp_nEvent -> Get("ncoll_mb");
     
     //anlysis1. projection to get eta yield
     //projectionX -> num of particle by eta
@@ -48,12 +56,14 @@ void analysis_dNdeta_ver2ver3MC_compare()
     TH1D *h1eta_pion0_ver3_test = (TH1D*)h2pTeta_pion0_ver3_test -> ProjectionX("h1eta_pion0_ver3_test");
     TH1D *h1eta_pion0_ver3_test_2e7events = (TH1D*)h2pTeta_pion0_ver3_test_2e7events -> ProjectionX("h1eta_pion0_ver3_test_2e7events");
     TH1D *h1eta_pion0_ver3_3e7events = (TH1D*)h2pTeta_pion0_ver3_3e7events -> ProjectionX("h1eta_pion0_ver3_3e7events");
+    TH1D *h1eta_pion0_ver3_tmp = (TH1D*)h2pTeta_pion0_ver3_tmp -> ProjectionX("h1eta_pion0_ver3_tmp");
 
     TH1D *h1eta_dir_ver2 = (TH1D*)h2pTeta_dir_ver2 -> ProjectionX("h1eta_dir_ver2");
     TH1D *h1eta_dir_ver3 = (TH1D*)h2pTeta_dir_ver3 -> ProjectionX("h1eta_dir_ver3");
     TH1D *h1eta_dir_ver3_test = (TH1D*)h2pTeta_dir_ver3_test -> ProjectionX("h1eta_dir_ver3_test");
     TH1D *h1eta_dir_ver3_test_2e7events = (TH1D*)h2pTeta_dir_ver3_test_2e7events -> ProjectionX("h1eta_dir_ver3_test_2e7events");
     TH1D *h1eta_dir_ver3_3e7events = (TH1D*)h2pTeta_dir_ver3_3e7events -> ProjectionX("h1eta_dir_ver3_3e7events");
+    TH1D *h1eta_dir_ver3_tmp = (TH1D*)h2pTeta_dir_ver3_tmp -> ProjectionX("h1eta_dir_ver3_tmp");
     
     //analysis2. scailing
     // a. event Num Scaling
@@ -64,6 +74,7 @@ void analysis_dNdeta_ver2ver3MC_compare()
     double nevent_ver3_test = ncoll_ver3_test -> Integral();
     double nevent_ver3_test_2e7events = ncoll_ver3_test_2e7events -> Integral();
     double nevent_ver3_3e7events = ncoll_ver3_3e7events -> Integral();
+    double nevent_ver3_tmp = ncoll_ver3_tmp -> Integral();
 
     double binwidth = h1eta_pion0_ver2 -> GetBinWidth(1);
 
@@ -72,6 +83,7 @@ void analysis_dNdeta_ver2ver3MC_compare()
     double scalar_ver3_test = (1./(nevent_ver3_test * binwidth));
     double scalar_ver3_test_2e7events = (1./(nevent_ver3_test_2e7events * binwidth));
     double scalar_ver3_3e7events = (1./(nevent_ver3_3e7events * binwidth));
+    double scalar_ver3_tmp = (1./(nevent_ver3_tmp * binwidth));
 
     h1eta_pion0_ver2 -> Scale(scalar_ver2);
     h1eta_dir_ver2 -> Scale(scalar_ver2);
@@ -87,6 +99,10 @@ void analysis_dNdeta_ver2ver3MC_compare()
 
     h1eta_pion0_ver3_3e7events -> Scale(scalar_ver3_3e7events);
     h1eta_dir_ver3_3e7events -> Scale(scalar_ver3_3e7events);
+
+    h1eta_pion0_ver3_tmp -> Scale(scalar_ver3_tmp);
+    h1eta_dir_ver3_tmp -> Scale(scalar_ver3_tmp);
+
 
     //output
     //------
@@ -107,10 +123,9 @@ void analysis_dNdeta_ver2ver3MC_compare()
     h1eta_pion0_ver3_3e7events -> Write();
     h1eta_dir_ver3_3e7events -> Write();
 
+    h1eta_pion0_ver3_tmp -> Write();
+    h1eta_dir_ver3_tmp -> Write();
 
     output -> Close();
-
-
-
 
 }
